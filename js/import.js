e87f52d69231e02fd5694915a72e78c20042896e
avyayrkashyap <47828169+avyayrkashyap@users.noreply.github.com>
2019-08-24 17:07:52 +0530

    Add files via upload

---
+++ js/import.js
@@ -0,0 +1,42 @@
+d3.csv("data/rainfall in india 1901-2015.csv", function(data){
+
+    console.log(data);
+
+    
+    data.forEach(function(d){
+        d["JAN"] = parseFloat(d["JAN"]);  // convert the number to float
+        d["FEB"] = parseFloat(d["FEB"]);
+        d["MAR"] = parseFloat(d["MAR"]);
+        d["APR"] = parseFloat(d["APR"]);
+        d["MAY"] = parseFloat(d["MAY"]);
+        d["JUN"] = parseFloat(d["JUN"]);
+        d["JUL"] = parseFloat(d["JUL"]);
+        d["AUG"] = parseFloat(d["AUG"]);
+        d["SEP"] = parseFloat(d["SEP"]);
+        d["OCT"] = parseFloat(d["OCT"]);
+        d["NOV"] = parseFloat(d["NOV"]);
+        d["DEC"] = parseFloat(d["DEC"]);
+        d["ANNUAL"] = parseFloat(d["ANNUAL"]);
+        
+        d["Jan-Feb"] = parseFloat(d["Jan-Feb"]);
+        d["Mar-May"] = parseFloat(d["Mar-May"]);
+        d["Jun-Sep"] = parseFloat(d["Jun-Sep"]);
+        d["Oct-Dec"] = parseFloat(d["Oct-Dec"]);
+
+        d["YEAR"] = parseFloat(d["YEAR"]);
+    })
+
+    
+	var Result = [];
+	for(var i in data){
+		
+		if(data[i]["SUBDIVISION"]=="PUNJAB"){
+			Result.push(data[i])
+		}
+		
+	}
+	
+	console.log(Result);
+    
+	
+});
